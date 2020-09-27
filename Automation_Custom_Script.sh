654f92038c61ccfc8c08280112c9f5a06535c321
Thomas Hollier <hollierthomas@gmail.com>
2020-09-26 18:43:07 -0700

    cleaned up and reordered script

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -1,50 +1,60 @@
+cd /root/
 
-# run vnc server in service mode which allows
-# screen sharing of the whole machine
-# and then run in direct capture mode
+echo ""
+echo "--------   Installing raspi config, utils, python & pip, libs for Pillow"
+echo ""
+apt-get update
+apt-get -y  install raspi-config p7zip-full vim python3 python3-pip libatlas-base-dev libopenjp2-7
+
+
+echo ""
+echo "--------   Run vnc server in service mode and create direct capture config"
+echo ""
+cat << EOF1  > /root/.vnc/config.d/vncserver-x11
+_LastUpdateCheckTime=int64:01d68ecd9e4d66ba
+CaptureTech=raspi
+EOF1
 echo "starting vncserver-x11-serviced"
 systemctl disable vncserver.service
 systemctl disable vncserver-virtuald.service
 systemctl enable vncserver-x11-serviced.service
+systemctl start vncserver-x11-serviced.service
 
-cat  << EOF  > /root/.vnc/config.d/vncserver-x11
-_LastUpdateCheckTime=int64:01d68ecd9e4d66ba
-CaptureTech=raspi
-EOF
 
-systemctl start  vncserver-x11-serviced.service
-
-echo "installing raspi config, utils and python"
-apt-get update
-apt-get  -y  install raspi-config p7zip-full vim python3 python3-pip libatlas-base-dev libopenjp2-7
-
-cd /root/
-echo "downloadibng pi sugar"
+echo ""
+echo "--------   Downloading pi sugar and starting service"
+echo ""
 curl http://cdn.pisugar.com/release/Pisugar-power-manager.sh | sudo bash
 sudo systemctl daemon-reload
 sudo systemctl enable pisugar-server
 sudo systemctl start pisugar-server
 
 
+echo ""
+echo "--------   Downloading LCD-HAT-Code not needed"
+echo ""
 # https://github.com/notro/fbtft
-echo "Downloading LCD-HAT-Code"
-wget https://www.waveshare.com/w/upload/6/63/1.3inch_LCD_HAT.7z
-
-echo "Extracting LCD-HAT-Code"
-7za x 1.3inch_LCD_HAT.7z
+# wget https://www.waveshare.com/w/upload/6/63/1.3inch_LCD_HAT.7z
+# 7za x 1.3inch_LCD_HAT.7z
 
 
+echo ""
+echo "--------   Install python packages"
+echo ""
 python3 -m pip install Pillow spidev numpy 
-#################
-#### Install script
-#################
-cp -r /boot/browniepi /root/
 
+
+
+echo ""
+echo "--------   Install scripts and start services"
+echo ""
+
+cp -r /boot/browniepi /root/
 
 #################
 #### setup service to launch browniepi process
 #################
-cat << EOF > /etc/systemd/system/browniepi.service
+cat << EOF2 > /etc/systemd/system/browniepi.service
 [Unit]
 Description=Launch the browniePi
 After=multi-user.target
@@ -55,15 +65,15 @@ ExecStart=/root/browniepi/start_browniePi.sh
 
 [Install]
 WantedBy=browniepi.target
-EOF
+EOF2
 
-cat << EOF > /etc/systemd/system/browniepi.target
+cat << EOF3 > /etc/systemd/system/browniepi.target
 [Unit]
 Description=Target for starting the browniepi
 Requires=multi-user.target
 After=multi-user.target
 AllowIsolate=yes
-EOF
+EOF3
 
 mkdir /etc/systemd/system/browniepi.target.wants
 ln -s /etc/systemd/system/browniepi.service /etc/systemd/system/browniepi.target.wants/browniepi.service
